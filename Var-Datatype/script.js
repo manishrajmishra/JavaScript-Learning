1c13c4432e0cb67412468d91c422e4c292e7fbfc
Manish Raj <rajmanish6851@gmail.com>
2020-12-27 23:28:31 +0530

    added the different ways to display the output

--- Var-Datatype/script.js
+++ Var-Datatype/script.js
@@ -14,3 +14,26 @@ paymentMode = "Credit Card" //Already declared
 
 //var if = 23; //Reserved keyword so its throwing error
 // There are reserved keywords which cannot be used as the variable name
+
+const uid = "abc123"; //Value cannot be changed
+
+// uid = "qwre"; //This will throw an errro because we have already defined and initalize the variable with the value
+
+var fullName = "Manish Raj";
+var email = "manishraj@gamil.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFormGoogle = false;
+
+// full_Name = alert("What is your name?"); //For taking input
+
+console.log(uid); //For displaying the output on the console
+
+// Other method of displaying the output
+console.log(`
+    With Unique ID: ${uid}
+    User is: ${fullName}
+    and his email is: ${email}
+    and uses the password: ${password}
+`);
