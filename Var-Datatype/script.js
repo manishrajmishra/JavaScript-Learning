db83f307c804a89c4c8385e51a265fe12c2f069b
Manish Raj <rajmanish6851@gmail.com>
2020-12-27 23:11:10 +0530

    added the concepts of variables and datatypes

---
+++ Var-Datatype/script.js
@@ -0,0 +1,16 @@
+var fullName = "Manish Raj"; //string
+
+var courseName = "React JS Bootcamp"; //string
+
+var isLoggedIn = false; //boolean
+
+var loggedCount = 34; //Integer
+
+var paymentMode;
+
+paymentMode = "Credit Card" //Already declared
+
+// Undefined is a datatype is JS
+
+var if = 23; //Reserved keyword so its throwing error
+// There are reserved keywords which cannot be used as the variable name
