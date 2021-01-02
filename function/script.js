9e06659b09559aa596a6787b8df55dfa80b5b16c
Manish Raj <rajmanish6851@gmail.com>
2021-01-02 23:14:12 +0530

    added few examples of function

---
+++ function/script.js
@@ -0,0 +1,18 @@
+function sayHello(name){
+    console.log("Hello there, Manish");
+    console.log("Hello there ", name);
+    console.log(`Hello Manish , ${name} said`);
+}
+
+sayHello("manish");
+sayHello("Avnish");
+
+
+function namstey(){
+    return "Hello India";
+}
+
+
+var reply = namstey();
+
+console.log(reply);
