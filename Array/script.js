2e728956d91ac79eca82e9a7976b056797151785
Manish Raj <rajmanish6851@gmail.com>
2021-01-03 12:37:53 +0530

    some basic examples of array

---
+++ Array/script.js
@@ -0,0 +1,21 @@
+var countries = ["India", "USA", "Japan", "Russia"];
+
+var states = new Array("Jharkhand", "Delhi", "Rajasthan");
+
+console.log(states[1]);
+
+console.log(states.length);
+
+states[0] = "Punjab";
+
+var user = ["Manish", "manishraj@gmail.com", 3, 43, true];
+
+user.pop();
+
+user.pop();
+
+console.log(user);
+
+console.log(user.indexOf("manish"));
+
+console.log(Array.from("manish"));
