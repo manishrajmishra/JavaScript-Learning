ee826d6a3392d8bb3760427ac69b4f83950f20b1
Manish Raj <rajmanish6851@gmail.com>
2021-01-03 13:27:11 +0530

    slice and splice examples with code

---
+++ Array/script3.js
@@ -0,0 +1,6 @@
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+console.log(users.slice(2, 3));
+
+var user = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+user.splice(1, 2, "Raj");
+console.log("user");
