a0190aad1ff288ece74bdb7e9d3f2f18090858fc
Manish Raj <rajmanish6851@gmail.com>
2021-01-03 13:20:56 +0530

    fill and filter examples with code and theory

---
+++ Array/script2.js
@@ -0,0 +1,6 @@
+var testArray = [2, 4, 6, 3, 1, 5, 9, 8];
+console.log(testArray.fill("h", 2, 5));
+
+const myNumber = [23, 24, 25, 66, 77, 87, 98];
+const result = myNumber.filter((num) => num > 55);
+console.log(result);
