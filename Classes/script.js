5882965458525837e1d09107347e2a2e4cb6bafb
Manish Raj <rajmanish6851@gmail.com>
2021-01-08 18:36:45 +0530

    inheritance concept code

--- Classes/script.js
+++ Classes/script.js
@@ -17,6 +17,25 @@ class User{
     getCourseList(){
         return this.#courseList;
     }
+
+    login(){
+        return "You are Logged In";
+    }
+}
+
+class SubAdmin extends User{
+    constructor(name, email){
+        super(name, email);
+    }
+    getAdminInfo(){
+        return "I am SubAdmin";
+    }
 }
 
 module.exports = User;
+
+const tom = new SubAdmin("tom", "tom@gmail.com");
+console.log(.getAdminInfo());
+console.log(tom.login());
+
+console.log(tom.getInfo());
