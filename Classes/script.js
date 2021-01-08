4a74d1dbf8a204a09a0496e0ab560870bf3e402d
Manish Raj <rajmanish6851@gmail.com>
2021-01-08 18:31:55 +0530

    made courseList a private variable

--- Classes/script.js
+++ Classes/script.js
@@ -3,18 +3,19 @@ class User{
         this.name = name;
         this.email = email;
     }
-    courseList = [];
+    //declaring as private variable
+    #courseList = [];
 
     getInfo(){
         return {name: this.name, email: this.email}
     }
 
     enrollCourse(name){
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
 
     getCourseList(){
-        return this.courseList;
+        return this.#courseList;
     }
 }
 
